b7e381cf4dc0acbb87fb2e2b3204f267c9def84b
Fathis, Zaqi <zaqi.fathis@htw-dresden.de>
2025-10-22 15:50:17 +0200

    Attribute facet-WIP

--- src/main/java/de/openfabtwin/bimserver/checkingservice/model/facet/Attribute.java
+++ src/main/java/de/openfabtwin/bimserver/checkingservice/model/facet/Attribute.java
@@ -12,12 +12,24 @@ public class Attribute extends Facet {
     private final Value value;
     private Cardinality cardinality;
     private final String instructions;
+    private final String applicability_templates;
+    private final String requirement_templates;
+    private final String prohibited_templates;
 
     public Attribute(Value name, Value value, String cardinality, String instructions){
         this.name = name;
         this.value = value;
         this.cardinality = cardinalityFromString(cardinality);
         this.instructions = instructions;
+        this.applicability_templates = extractValue(value, false).isEmpty() ?
+                "Data where the " + name + " is provided" :
+                "Data where the " + name + " is " + extractValue(value, false);
+        this.requirement_templates = extractValue(value, false).isEmpty() ?
+                "The " + name + " shall be provided" :
+                "The " + name + " shall be " + extractValue(value, false);
+        this.prohibited_templates = extractValue(value, false).isEmpty() ?
+                "The " + name + " shall not be provided" :
+                "The " + name + " shall not be " + extractValue(value, false);
     }
 
     @Override
