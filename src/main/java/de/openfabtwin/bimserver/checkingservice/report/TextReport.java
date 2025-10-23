003c57e3b74ccd6eff196bd4e9fd71acd147a8cc
zaqi <zaqifathis@gmail.com>
2025-10-23 07:19:08 +0200

    yext report setup

---
+++ src/main/java/de/openfabtwin/bimserver/checkingservice/report/TextReport.java
@@ -0,0 +1,15 @@
+package de.openfabtwin.bimserver.checkingservice.report;
+
+import de.openfabtwin.bimserver.checkingservice.model.Ids;
+
+public class TextReport extends Reporter {
+
+    public TextReport(Ids ids) {
+        super(ids);
+    }
+
+    @Override
+    public void report() {
+
+    }
+}
