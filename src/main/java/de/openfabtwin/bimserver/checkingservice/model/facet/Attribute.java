6d50c7d2cd3ed12eb011f50727ffc378f19b4f34
Fathis, Zaqi <zaqi.fathis@htw-dresden.de>
2025-10-29 16:56:45 +0100

    attribute done

--- src/main/java/de/openfabtwin/bimserver/checkingservice/model/facet/Attribute.java
+++ src/main/java/de/openfabtwin/bimserver/checkingservice/model/facet/Attribute.java
@@ -1,7 +1,7 @@
 package de.openfabtwin.bimserver.checkingservice.model.facet;
 
-import de.openfabtwin.bimserver.checkingservice.model.SimpleValue;
 import de.openfabtwin.bimserver.checkingservice.model.Value;
+import de.openfabtwin.bimserver.checkingservice.model.result.AttributeResult;
 import de.openfabtwin.bimserver.checkingservice.model.result.Result;
 import org.bimserver.emf.IdEObject;
 import org.bimserver.emf.IfcModelInterface;
@@ -9,6 +9,7 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Attribute extends Facet {
@@ -28,9 +29,9 @@ public class Attribute extends Facet {
             this.requirement_templates = "The " + name.extract() + " shall be provided";
             this.prohibited_templates = "The " + name.extract() + " shall not be provided";
         } else {
-            this.applicability_templates = "Data where the " + name + " is " + value.extract();
-            this.requirement_templates = "The " + name + " shall be " + value.extract();
-            this.prohibited_templates = "The " + name + " shall not be " + value.extract();
+            this.applicability_templates = "Data where the " + name.extract() + " is " + value.extract();
+            this.requirement_templates = "The " + name.extract() + " shall be " + value.extract();
+            this.prohibited_templates = "The " + name.extract() + " shall not be " + value.extract();
         }
     }
 
@@ -62,7 +63,103 @@ public class Attribute extends Facet {
     @Override
     public Result matches(IdEObject element) {
 
-        return null;
+        List<EAttribute> features = new ArrayList<>();
+        for (EAttribute a : element.eClass().getEAllAttributes()) {
+            if (name.matches(a.getName())) {
+                features.add(a);
+            }
+        }
+
+        return switch (this.cardinality) {
+            case REQUIRED    -> evalRequired(element, features);
+            case OPTIONAL    -> evalOptional(element, features);
+            case PROHIBITED  -> evalProhibited(element, features);
+        };
+    }
+
+    private Result evalProhibited(IdEObject element, List<EAttribute> attributes) {
+        if(attributes.isEmpty()) {
+            return pass();
+        } else {
+            for (EAttribute attr: attributes) {
+                Object raw = element.eGet(attr);
+                if(isPresent(raw, attr)) return fail("PROHIBITED");
+            }
+            return fail("PROHIBITED");
+        }
+    }
+
+    private Result evalOptional(IdEObject element, List<EAttribute> attributes) {
+        if(attributes.isEmpty()) return pass();
+
+        for (EAttribute attr: attributes) {
+            Object raw = element.eGet(attr);
+            if(!isPresent(raw, attr)) continue;
+            if (!isActualValue(raw)) return fail(Map.of(
+                    "type", "FALSEY",
+                    "actual", raw.toString()
+            ));
+            if (this.value != null && !value.matches(raw.toString().trim())) {
+                return fail(Map.of(
+                        "type", "VALUE",
+                        "actual", raw.toString()
+                ));
+            }
+        }
+        return pass();
+    }
+
+    private Result evalRequired(IdEObject element, List<EAttribute> attributes) {
+        if (attributes.isEmpty()) return fail("NOVALUE");
+
+        for(EAttribute attr : attributes) {
+            Object raw = element.eGet(attr);
+            if(!isPresent(raw, attr)) return fail("NOVALUE");
+            if (!isActualValue(raw)) return fail(Map.of(
+                    "type", "FALSEY",
+                    "actual", raw.toString()
+            ));
+            if (this.value != null && !value.matches(raw.toString().trim())) {
+                return fail(Map.of(
+                        "type", "VALUE",
+                        "actual", raw.toString()
+                ));
+            }
+        }
+        return pass();
+    }
+
+    private boolean isPresent(Object raw, EAttribute attr) {
+        if (raw == null) return false;
+
+        if(attr.isMany()) {
+            List<Object> list = (List<Object>) raw;
+            return !list.isEmpty();
+        }
+        return true;
+    }
+
+    private boolean isActualValue(Object raw) {
+        if (raw instanceof IdEObject) return false;
+        if (raw instanceof CharSequence cs) {
+            return !cs.toString().trim().isEmpty();
+        }
+        if (raw instanceof Enum<?> en) {
+            return !"UNKNOWN".equalsIgnoreCase(en.name());
+        }
+        if (raw instanceof Collection<?> col) {
+            return !col.isEmpty();
+        }
+        if (raw.getClass().isArray()) {
+            return Array.getLength(raw) > 0;
+        }
+        return true;
+    }
+
+    private static AttributeResult pass() { return new AttributeResult(true, null); }
+    private static AttributeResult fail(String type) { return new AttributeResult(false, Map.of("type", type)); }
+    private static AttributeResult fail(Map<String, Object> reason) {
+        return new AttributeResult(false, reason);
     }
 
 }
--- src/main/java/de/openfabtwin/bimserver/checkingservice/model/result/AttributeResult.java
+++ src/main/java/de/openfabtwin/bimserver/checkingservice/model/result/AttributeResult.java
@@ -10,6 +10,16 @@ public class AttributeResult extends Result {
 
     @Override
     public String to_String() {
-        return "Attribute result: " + (isPass ? "Pass" : "Fail") + ". Reason: " + reason;
+        if (reason.get("type") == "NOVALUE")
+            return "The required attribute did not exist";
+        else if (reason.get("type") == "FALSEY")
+            return "The attribute value" + reason.get("actual") + " is empty";
+        else if (reason.get("type") == "INVALID")
+            return "An invalid attribute name was specified in the IDS";
+        else if (reason.get("type") == "VALUE")
+            return "The attribute value " + reason.get("actual") +" does not match the requirement";
+        else if (reason.get("type") == "PROHIBITED")
+            return "The attribute value should not have met the requirement";
+        return "";
     }
 }
